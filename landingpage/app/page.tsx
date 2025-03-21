eb9e55d74357336158af97ac9eb0d80b8a272e3e
Thomas <66449641+thomahn3@users.noreply.github.com>
2025-03-21 16:03:50 +1000

    responsive design

--- landingpage/app/page.tsx
+++ landingpage/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center w-full">
         <div className="flex flex-col md:flex-row items-center w-full">
         <div className="p-6 md:w-1/2">
-          <div className="ml-20 mr-30"> {/* Make this dynamic for different screen sizes */}
+          <div className="ml-5 mr-5 md:ml-20 md:mr-30"> {/* Make this dynamic for different screen sizes */}
           <p className="text-green-500">Customer Convenience</p>
           <p className="text-xl font-bold mt-2">Make Searching Effortless</p>
           <p className="mt-4 text-slate-500">
@@ -48,19 +48,19 @@ export default function Home() {
           </p>
           <div className="mt-6 space-y-6 flex flex-col">
             <div className="flex flex-row items-start">
-            <Waypoints size={24} strokeWidth={2} className="mr-4 flex-none mt-[20px]" />
+            <Waypoints size={24} strokeWidth={2} className="mr-4 flex-none mt-[50px] md:mt-[20px]" />
             <p className="text-slate-500">
               We facilitate the seamless connection between customers and businesses, offering a swift and effortless solution to the complexities of personally finding services.
             </p>
             </div>
             <div className="flex flex-row items-start">
-            <UserRoundSearch size={24} strokeWidth={2} className="mr-4 flex-none mt-[20px]" />
+            <UserRoundSearch size={24} strokeWidth={2} className="mr-4 flex-none mt-[39px] md:mt-[20px]" />
             <p className="text-slate-500">
               Access local experts within your vicinity, supporting local businesses while ensuring that the quality of the service you require is exceptional.
             </p>
             </div>
             <div className="flex flex-row items-start">
-            <CalendarDays size={24} strokeWidth={2} className="mr-4 flex-none mt-[20px]" />
+            <CalendarDays size={24} strokeWidth={2} className="mr-4 flex-none mt-[47px] md:mt-[20px]" />
             <p className="text-slate-500">
               Customers can conveniently book and schedule services directly through the application, offering options to select preferred times and dates, enhancing customer convenience.
             </p>
@@ -68,7 +68,7 @@ export default function Home() {
           </div>
           </div>
         </div>
-        <div className="p-6 md:w-1/2">
+        <div className="p-6 md:w-1/2 flex justify-center items-center">
         <img src="/person_on_the_couch_with_laptop.svg" alt="Person on the couch with laptop illustration" className="w-3/4"/>
         </div>
         </div>
@@ -82,7 +82,7 @@ export default function Home() {
           <img src="/painter_decorating.svg" alt="painter decorating" className="w-3/4"/>
         </div>
         <div className="p-6 md:w-1/2">
-          <div className="ml-20 mr-20">
+          <div className="ml-5 mr-5 md:ml-20 md:mr-20">
           <p className="text-green-500">Business Reach</p>
           <p className="text-xl font-bold mt-2">Increase Discoverability</p>
           <p className="mt-4 text-slate-500">
@@ -90,19 +90,19 @@ export default function Home() {
           </p>
           <div className="mt-6 space-y-6">
             <div className="flex flex-row items-start">
-            <UsersRound size={24} strokeWidth={2} className="mr-4 flex-none mt-[20px]" />
+            <UsersRound size={24} strokeWidth={2} className="mr-4 flex-none mt-[50px] md:mt-[20px]" />
             <p className="text-slate-500">
               Service providers gain access to a large pool of potential customers within their local vicinity, facilitating businesses in attracting new clients without the necessity of extensive advertising campaigns.
             </p>
             </div>
             <div className="flex flex-row items-start">
-            <CalendarSync size={24} strokeWidth={2} className="mr-4 flex-none mt-[35px]" />
+            <CalendarSync size={24} strokeWidth={2} className="mr-4 flex-none mt-[60px] md:mt-[35px]" />
             <p className="text-slate-500">
               The application automates the customer connection process, thereby reducing the time and effort allocated to marketing, quotation, and scheduling, enabling businesses to focus on delivering their services rather than managing leads.
             </p>
             </div>
             <div className="flex flex-row items-start">
-            <HandCoins size={24} strokeWidth={2} className="mr-4 flex-none mt-[20px]" />
+            <HandCoins size={24} strokeWidth={2} className="mr-4 flex-none mt-[40px] md:mt-[20px]" />
             <p className="text-slate-500">
               Service providers have the opportunity to accept additional work commitments during evenings, weekends, or public holidays, thereby enhancing their earning potential and offering flexablity.
             </p>
@@ -116,7 +116,7 @@ export default function Home() {
       <section className="py-20 border-t border-gray-200">
         <div className="flex flex-col justify-center items-center w-full">
           <div className="flex flex-col md:flex-row items-center w-full">
-            <div className="p-6 md:w-1/2">
+            <div className="p-6 md:w-1/2 order-last md:order-first">
               <div className="ml-20 mr-20">
                 <p className="text-xl font-bold mt-2">Want to get in touch?</p>
                 <p>Contact us at <a href="mailto:thomas.e.mahne@gmail.com" className="underline">thomas.e.mahne@gmail.com</a>.</p>
