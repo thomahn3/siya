4fa89349fa9bf8dcf6a2f53779a29ec49a4375a8
Thomas <66449641+thomahn3@users.noreply.github.com>
2025-03-24 19:06:08 +1000

    changes

--- landingpage/middleware.ts
+++ landingpage/middleware.ts
@@ -1,14 +1,1 @@
-import jwt from 'jsonwebtoken';
-
-export default function requireAuth(req:, res, next) {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
-    try {
-      jwt.verify(token, process.env.SECRET_KEY);
-      next();
-    } catch (err) {
-      res.status(401).json({ message: 'Unauthorized' });
-    }
-  }
+export { auth as middleware } from "@/auth"
