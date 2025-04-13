79b7e7b2d937973fa3c1d5833aa88dec03547476
Thomas <66449641+thomahn3@users.noreply.github.com>
2025-04-13 19:38:04 +1000

    Update route.ts

--- landingpage/app/api/get-subscribers/route.ts
+++ landingpage/app/api/get-subscribers/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-import { decryptEmail } from "../../../utils/encryption";
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  try {
-    const subscribers: { email: string }[] = await prisma.subscriber.findMany();
-
-    const emails = subscribers.map(subscriber => decryptEmail(subscriber.email));
-
-    return NextResponse.json({ success: true, emails });
-  } catch (error) {
-    console.error("Subscription error:", error);
-    return NextResponse.json({ error: "Failed to retrieve emails" }, { status: 500 });
-  }
-}
+//import { NextResponse } from "next/server";
+//import { PrismaClient } from "@prisma/client";
+//import { decryptEmail } from "../../../utils/encryption";
+//
+//const prisma = new PrismaClient();
+//
+//export async function GET() {
+//  try {
+//    const subscribers: { email: string }[] = await prisma.subscriber.findMany();
+//
+//    const emails = subscribers.map(subscriber => decryptEmail(subscriber.email));
+//
+//    return NextResponse.json({ success: true, emails });
+//  } catch (error) {
+//    console.error("Subscription error:", error);
+//    return NextResponse.json({ error: "Failed to retrieve emails" }, { status: 500 });
+//  }
+//}
