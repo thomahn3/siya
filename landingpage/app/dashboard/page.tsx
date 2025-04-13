8bc5b5aba2149378f4a79d42c1cde00b152f658f
Thomas <66449641+thomahn3@users.noreply.github.com>
2025-04-13 17:24:51 +1000

    misc

--- landingpage/app/dashboard/page.tsx
+++ landingpage/app/dashboard/page.tsx
@@ -9,13 +9,13 @@ const Page = async () => {
     const session = await auth();
     if (!session) {
         redirect('/sign-in')
-    } //else if (session && !(await checkProfileSetup(session.user?.id))) {
-        //redirect("/profile-setup")
-    //}
+    } else if (session && !(await checkProfileSetup(session.user?.id))) {
+        redirect("/profile-setup")
+    }
 
     return (
         <div className="flex flex-col min-h-[calc(100vh-4rem)] items-center justify-center py-2">
-        <h1 className="text-4xl font-bold">Dashbaord</h1>
+        <h1 className="text-4xl font-bold">Dashboard</h1>
         <p className="mt-4 text-lg">This is the dashboard page.</p>
         <p className="mt-4 text-lg">Your Email: {session.user?.email}</p>
         <SignOut />
