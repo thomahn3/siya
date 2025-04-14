1cfd575cd9f520d93f6acb1b1fd092068f6afb77
Thomas <66449641+thomahn3@users.noreply.github.com>
2025-04-14 18:57:48 +1000

    stable prod build

--- landingpage/app/api/subscribe/route.ts
+++ landingpage/app/api/subscribe/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { encryptEmail } from "../../../utils/encryption";
 
 const prisma = new PrismaClient();
 
@@ -9,10 +8,8 @@ export async function POST(req: Request) {
     const { email } = await req.json();
     if (!email) return NextResponse.json({ error: "Email is required" }, { status: 400 });
 
-    const encryptedEmail = encryptEmail(email);
-
     await prisma.subscriber.create({
-      data: { email: encryptedEmail },
+      data: { email: email },
     });
 
     return NextResponse.json({ success: true, message: "Email subscribed successfully!" });
